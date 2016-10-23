ea304e5c6a9bd4301d255285a0624626bccd1add
gowtham <gowhamreddy@gmail.com>
2016-10-23 18:57:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config= {
+    user:'gauthy',
+    database:'gauthy',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -23,6 +31,21 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+  //make a select request
+  pool.query('SELECT * from test',function(err,result){
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          res.send(JSON.stringify(result));
+      }
+  });
+  //make a request response
 });
 
 var counter=0;
