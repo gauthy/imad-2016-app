7aa4e86f8ea3bf6e410503b5d34e665f51a2bc5f
gowtham <gowhamreddy@gmail.com>
2016-11-11 16:00:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ function createTemplate (data) {
       <body>
           <div class="container">
               <div>
-                  <a href="/">Home</a>
+                  <a href="/" class="nav-link1">Home</a>
               </div>
               <hr/>
               <div class="commentsec">
