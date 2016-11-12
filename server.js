d5ed66412bec9cd845940c314f0f5dac2935c3f9
gowtham <gowhamreddy@gmail.com>
2016-11-12 12:17:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ function createTemplate (data) {
                   ${date.toDateString()}
               </div>
               <div>
-              <p>  ${context}</p>
+                ${content}
               </div>
               </div>
               <hr/>
