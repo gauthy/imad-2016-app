09d0151e75815516128483e5a67658c9f50f248f
gowtham <gowhamreddy@gmail.com>
2016-11-19 10:10:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -82,8 +82,8 @@ function loadLoginForm () {
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
-        <h3> Hi <i>${username}</i></h3>
-        <a href="/logout">Logout</a>
+        <h3 > Hello <strong>${username}</strong></h3>
+        <a href="/logout" class="buton">Logout</a>
     `;
 }
 
