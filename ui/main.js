db928ea5734d16dbd5ac4d1c7deeb2c8b0d53171
gowtham <gowhamreddy@gmail.com>
2016-12-09 10:49:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 function loadLoginForm () {
     var loginHtml = `
         <h3>Login/Signup here friends.</h3>
-        <input type="text" id="username" placeholder="username" style="background-color:#33FFF9  ; color:Lime;" />
+        <input type="text" id="username" placeholder="username" style="background-color:#33FFF9  ; color:Lime;margin-radius:10%; " />
         <input type="password" id="password" placeholder="password" style="background-color:#33FFF9  ; color:Lime;" />
         <br/><br/>
         <input type="submit" id="login_btn" value="Login" class="buton"/>
